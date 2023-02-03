aa702fe33b5c928b2f935674e02fe1dcc337a85b
senzoo-oh <osj3418@gmail.com>
2023-02-03 22:54:21 +0900

    백준 #2581
    백준 단계별 기초수학2

---
+++ 기본수학2/2581.py
@@ -0,0 +1,17 @@
+M = int(input())
+N = int(input())
+list = []
+
+for i in range(M, N + 1) :
+    if i == 1 :
+        continue
+    for j in range(2, i) :
+        if i % j == 0 :
+            break
+    else :
+        list.append(i)
+if len(list) == 0 :
+    print(-1)
+else :
+    print(sum(list))
+    print(min(list))
